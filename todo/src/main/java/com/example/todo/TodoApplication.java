8295955628d38eb5440c010a2ccd42a0df041864
MaBi0110 <biziewskamk@gmail.com>
2023-07-11 16:46:29 +0200

    add EntityManager and DbTaskRepository

---
+++ todo/src/main/java/com/example/todo/TodoApplication.java
@@ -0,0 +1,26 @@
+package com.example.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class TodoApplication {
+
+    public static void main(String[] args) {
+
+        ConfigurableApplicationContext context = SpringApplication.run(TodoApplication.class, args);
+        TaskController taskController = context.getBean(TaskController.class);
+        taskController.loop();
+
+    }
+
+    @Bean
+    Scanner scanner() {
+        return new Scanner(System.in);
+    }
+
+}
