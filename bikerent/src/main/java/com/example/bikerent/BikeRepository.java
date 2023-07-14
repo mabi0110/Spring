dd0c84d0ebcc750d89570e17b630b3cff1f1053b
MaBi0110 <biziewskamk@gmail.com>
2023-07-14 06:47:45 +0200

    add CrudRepository

--- bikerent/src/main/java/com/example/bikerent/BikeRepository.java
+++ bikerent/src/main/java/com/example/bikerent/BikeRepository.java
@@ -1,31 +1,6 @@
 package com.example.bikerent;
 
-import jakarta.persistence.EntityManager;
-import org.springframework.stereotype.Repository;
 
+import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
-@Repository
-public class BikeRepository {
-
-    private final EntityManager entityManager;
-
-    public BikeRepository(EntityManager entityManager) {
-        this.entityManager = entityManager;
-
-    }
-
-    public void save(Bike bike){
-        entityManager.persist(bike);
-    }
-
-    public Optional<Bike> findById(Long id){
-        Bike bike = entityManager.find(Bike.class, id);
-        return Optional.ofNullable(bike);
-    }
-
-    public void deleteById(Long id){
-        findById(id).ifPresent(entityManager::remove);
-    }
-}
+interface BikeRepository extends CrudRepository<Bike, Long> { }
