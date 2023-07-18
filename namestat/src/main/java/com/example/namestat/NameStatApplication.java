cbed88d1f1c038a87a7ba21f3a192d147f09219b
MaBi0110 <biziewskamk@gmail.com>
2023-07-18 17:44:28 +0200

    add Ex1

---
+++ namestat/src/main/java/com/example/namestat/NameStatApplication.java
@@ -0,0 +1,13 @@
+package com.example.namestat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameStatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NameStatApplication.class, args);
+    }
+
+}
---
+++ namestat/src/main/java/com/example/namestat/NameStats.java
@@ -0,0 +1,53 @@
+package com.example.namestat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class NameStats {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer count;
+    private String gender;
+
+    public NameStats() {
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
