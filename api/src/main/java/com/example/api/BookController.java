137cf6a87cf26ceb8321b276d43af0b9bfbaf910
MaBi0110 <biziewskamk@gmail.com>
2023-08-05 17:00:09 +0200

    add produces = MediaType.APPLICATION_XML_VALUE

--- api/src/main/java/com/example/api/BookController.java
+++ api/src/main/java/com/example/api/BookController.java
@@ -1,16 +1,18 @@
 package com.example.api;
 
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
     List<Book> getAllBooks() {
         return List.of(
                 new Book("Harry Potter", "J. K. Rowling", 1997, 233),
