ebe473c13dfce7c80feb34c49525a1ce51d4a47d
MaBi0110 <biziewskamk@gmail.com>
2023-08-07 18:21:57 +0200

    add ShopApplication initial commit

---
+++ api-zmienne-parametry/src/main/java/com/example/shop/ProductRepository.java
@@ -0,0 +1,28 @@
+package com.example.shop;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class ProductRepository {
+
+    private final List<Product> products = new ArrayList<>();
+
+    public ProductRepository() {
+        products.add(
+                new Product(1, "mleko", 3.8, new Producer("Mlekpol", new Address("Warszawa", "20-022", "Wysoka 20")))
+        );
+        products.add(
+                new Product(2, "czekolada", 4.5, new Producer("Wedel", new Address("Warszawa", "02-154", "Aleje Jerozolimskie 150")))
+        );
+        products.add(
+                new Product(3, "kawa", 18.00, new Producer("Nescafe", new Address("Warszawa", "01-022", "Barcicka 12")))
+        );
+    }
+
+
+
+
+}
---
+++ api-zmienne-parametry/src/test/java/com/example/apizmienneparametry/ApiZmienneParametryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apizmienneparametry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiZmienneParametryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
