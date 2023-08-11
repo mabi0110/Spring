a731ce05e03b1d78386a83ecc7a31a1344792d85
mabi0110 <biziewskamk@gmail.com>
2023-08-11 08:00:39 +0200

    add Student, StudentRepository, StudentService, StudentController, StudentConfig

---
+++ simple-api/src/main/java/com/example/simpleapi/student/StudentConfig.java
@@ -0,0 +1,21 @@
+package com.example.simpleapi.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student jan = new Student("Jan", LocalDate.of(2000, 3, 1), 23, "jan@example.com");
+            Student ula = new Student("Ula", LocalDate.of(2001, 10, 12), 22, "ula@example.com");
+            studentRepository.saveAll(List.of(jan, ula));
+        };
+    }
+}
