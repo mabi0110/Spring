4e0f66555a88c605d1982165053467a4ac91731e
mabi0110 <biziewskamk@gmail.com>
2023-08-14 13:56:21 +0200

    add StudentRepository

--- jpa-course/src/main/java/com/example/jpacourse/Application.java
+++ jpa-course/src/main/java/com/example/jpacourse/Application.java
@@ -1,13 +1,24 @@
 package com.example.jpacourse;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student maria = new Student("Maria", "Nowak", "maria@example.com", 23);
+            studentRepository.save(maria);
+        };
     }
 
 }
