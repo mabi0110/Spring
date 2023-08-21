e405b1bdf47794991a5d787430e42f6c1afec8b3
mabi0110 <biziewskamk@gmail.com>
2023-08-21 13:31:55 +0200

    add directories and configuration

---
+++ strona-glowna/src/test/java/com/example/stronaglowna/StronaGlownaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stronaglowna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StronaGlownaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
