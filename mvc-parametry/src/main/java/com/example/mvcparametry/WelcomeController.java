692fa4fe181f795539e4172ddf4b457e73a3e1f6
mabi0110 <biziewskamk@gmail.com>
2023-08-21 14:21:44 +0200

    add @RequestHeader

--- mvc-parametry/src/main/java/com/example/mvcparametry/WelcomeController.java
+++ mvc-parametry/src/main/java/com/example/mvcparametry/WelcomeController.java
@@ -1,8 +1,8 @@
 package com.example.mvcparametry;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,7 +11,9 @@ public class WelcomeController {
 
     @GetMapping("/hello")
     @ResponseBody
-    String hello(@RequestParam(required = false, defaultValue = "stranger") String name) {
+    String hello(@RequestParam(required = false, defaultValue = "stranger") String name,
+                 @RequestHeader("User-Agent") String userAgent) {
+        System.out.println("User Agent klienta: " + userAgent);
         return "Hello " + name;
     }
 }
