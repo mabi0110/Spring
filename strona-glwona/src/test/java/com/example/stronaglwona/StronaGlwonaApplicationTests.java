5ff03d4b893db579db653f5b070f68c007055e72
mabi0110 <biziewskamk@gmail.com>
2023-08-21 12:28:54 +0200

    initial commit

---
+++ strona-glwona/src/test/java/com/example/stronaglwona/StronaGlwonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stronaglwona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StronaGlwonaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
