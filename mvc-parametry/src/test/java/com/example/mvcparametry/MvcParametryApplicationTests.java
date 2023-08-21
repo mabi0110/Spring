120cbd8cba151c07b91112e5911547af77698436
mabi0110 <biziewskamk@gmail.com>
2023-08-21 13:43:43 +0200

    initial commit

---
+++ mvc-parametry/src/test/java/com/example/mvcparametry/MvcParametryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvcparametry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcParametryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
