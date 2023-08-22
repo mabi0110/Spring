fc1ea2842a3090a4a89df0c7aea45c1b7cef26b8
mabi0110 <biziewskamk@gmail.com>
2023-08-22 15:15:34 +0200

    add number generator

---
+++ mvc/src/main/java/com/example/mvc/RandomNumberService.java
@@ -0,0 +1,17 @@
+package com.example.mvc;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class RandomNumberService {
+
+    private final Random random = new Random();
+
+    int generateRandomInRange(int start, int end) {
+        int range = end - start + 1;
+        int randomNumber = random.nextInt(range);
+        return start + randomNumber;
+    }
+}
