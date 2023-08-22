f49f316570567bc8614c94de10901b40beb12d61
mabi0110 <biziewskamk@gmail.com>
2023-08-22 11:52:44 +0200

    initial commit

---
+++ generator-tabel/src/main/java/com/example/generatortabel/TableGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.generatortabel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TableGeneratorApplication.class, args);
+    }
+
+}
---
+++ generator-tabel/src/test/java/com/example/generatortabel/GeneratorTabelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.generatortabel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratorTabelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
