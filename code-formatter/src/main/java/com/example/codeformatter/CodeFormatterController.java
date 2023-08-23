f023e3c35fd26507bd5534a2de2520d19a2f23ae
mabi0110 <biziewskamk@gmail.com>
2023-08-23 10:25:07 +0200

    add code formatter

---
+++ code-formatter/src/main/java/com/example/codeformatter/CodeFormatterController.java
@@ -0,0 +1,36 @@
+package com.example.codeformatter;
+
+import com.google.googlejavaformat.java.FormatterException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CodeFormatterController {
+
+    private final CodeFormatterService codeFormatterService;
+
+    public CodeFormatterController(CodeFormatterService codeFormatterService) {
+        this.codeFormatterService = codeFormatterService;
+    }
+
+
+    @GetMapping("/")
+    String home() {
+        return "index";
+    }
+
+    @PostMapping("/format")
+    String formatCode(@RequestParam String code, Model model){
+        try {
+            String formattedCode = codeFormatterService.formatCode(code);
+            model.addAttribute("formattedCode", formattedCode);
+        } catch (FormatterException e) {
+            return "errorPage";
+        }
+        model.addAttribute("unformattedCode", code);
+        return "codeResult";
+    }
+}
---
+++ code-formatter/src/main/java/com/example/codeformatter/CodeFormatterService.java
@@ -0,0 +1,14 @@
+package com.example.codeformatter;
+
+import com.google.googlejavaformat.java.Formatter;
+import com.google.googlejavaformat.java.FormatterException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CodeFormatterService {
+
+    String formatCode(String sourceCode) throws FormatterException {
+        return new Formatter().formatSource(sourceCode);
+    }
+
+}
