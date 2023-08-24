566b532031b8c45a85eee549306cd5b3a62ba4b2
mabi0110 <biziewskamk@gmail.com>
2023-08-24 10:54:26 +0200

    initial commit

---
+++ instrukcje-sterujace/src/main/java/com/example/instrukcjesterujace/ExampleController.java
@@ -0,0 +1,14 @@
+package com.example.instrukcjesterujace;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ExampleController {
+
+    @GetMapping
+    String example(Model model){
+        return "example";
+    }
+}
---
+++ instrukcje-sterujace/src/test/java/com/example/instrukcjesterujace/InstrukcjeSterujaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.instrukcjesterujace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstrukcjeSterujaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
