02e1a2bbed638012e24a09f67e6350db4ea0d003
mabi0110 <biziewskamk@gmail.com>
2023-08-24 14:50:08 +0200

    add text statistics app

---
+++ statystyki-tekstu/src/test/java/com/example/statystykitekstu/StatystykiTekstuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.statystykitekstu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatystykiTekstuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
