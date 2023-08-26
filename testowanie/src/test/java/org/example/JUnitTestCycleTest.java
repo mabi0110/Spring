4cdd88b4e3682792ce7a553504cd4bd7626864c8
mabi0110 <biziewskamk@gmail.com>
2023-08-26 16:15:16 +0200

    add @BeforeEach

--- testowanie/src/test/java/org/example/JUnitTestCycleTest.java
+++ testowanie/src/test/java/org/example/JUnitTestCycleTest.java
@@ -11,19 +11,17 @@ public class JUnitTestCycleTest {
 
     @BeforeEach
     public void init(){
+        System.out.println("init");
         random = new Random();
-        random.setSeed(12345L);
     }
 
     @Test
     public void test1(){
         System.out.println("Test 1 " + random.nextInt());
-
     }
 
     @Test
     public void test2(){
-
         System.out.println("Test 2 " + random.nextInt());
     }
 
