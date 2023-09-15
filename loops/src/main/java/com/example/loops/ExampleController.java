9d5b07d175ee9511c74f04e83802b0ef1a5aebf1
mabi0110 <biziewskamk@gmail.com>
2023-09-15 14:23:04 +0200

    display list of people

--- loops/src/main/java/com/example/loops/ExampleController.java
+++ loops/src/main/java/com/example/loops/ExampleController.java
@@ -4,12 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class ExampleController {
 
     @GetMapping("/")
     String example(Model model) {
-
+        List<Person> people = List.of(
+                new Person("Jan", "Nowak"),
+                new Person("Ala", "Nowak"),
+                new Person("Ula", "Nowak"),
+                new Person("Ola", "Nowak")
+        );
+        model.addAttribute("people", people);
         return "example";
     }
 }
