2287bd7b96c3f9e57034985e0866ebecb3964427
mabi0110 <biziewskamk@gmail.com>
2023-09-17 21:58:38 +0200

    add alert using th:utext

---
+++ thymeleaf-text/src/main/java/com/example/thymeleaftext/TextController.java
@@ -0,0 +1,15 @@
+package com.example.thymeleaftext;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TextController {
+
+    @GetMapping("/")
+    String home(Model model) {
+        model.addAttribute("dangerousCode", "<script>alert('Masz wirusa');</script>");
+        return "index";
+    }
+}
---
+++ thymeleaf-text/src/test/java/com/example/thymeleaftext/ThymeleafTextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleaftext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafTextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
