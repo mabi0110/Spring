6c52799a18cb1c5a4d05025b964f989de7887fb0
mabi0110 <biziewskamk@gmail.com>
2023-09-17 22:29:05 +0200

    add table

---
+++ thymeleaf-selekcja/src/main/java/com/example/expressions/ExpressionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.expressions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpressionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpressionsApplication.class, args);
+    }
+
+}
---
+++ thymeleaf-selekcja/src/main/java/com/example/expressions/HomeController.java
@@ -0,0 +1,24 @@
+package com.example.expressions;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    String home(Model model){
+        List<Product> products = List.of(
+                new Product("mleko", "mlekowita", 3.2),
+                new Product("czekolada", "czekoladex", 2.2),
+                new Product("jogurt", "jogurtex", 1.2),
+                new Product("chleb", "chleboteka", 4.1),
+                new Product("pasztet", "mięsny raj", 8.9)
+        );
+        model.addAttribute("products", products);
+        return "index";
+    }
+}
