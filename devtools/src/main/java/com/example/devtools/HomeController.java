ec73297fc0d6a129335dd4e3ed75e3a2c57c3426
mabi0110 <biziewskamk@gmail.com>
2023-09-17 21:21:08 +0200

    add spring.thymeleaf.cache=false

---
+++ devtools/src/main/java/com/example/devtools/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.devtools;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    String home(Model model){
+        model.addAttribute("message", "Tekst z kontrolera przed zmianą");
+        return "index";
+    }
+
+}
