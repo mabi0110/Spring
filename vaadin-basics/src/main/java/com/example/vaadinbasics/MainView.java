41524e41ca9da1c22bb99389ef17ca601b2ef799
mbiziewska <Kotmaaids2502!@>
2023-09-19 15:22:16 +0200

    add data binding

--- vaadin-basics/src/main/java/com/example/vaadinbasics/MainView.java
+++ vaadin-basics/src/main/java/com/example/vaadinbasics/MainView.java
@@ -5,6 +5,8 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -14,14 +16,12 @@ public class MainView extends VerticalLayout {
 
     public MainView(){
         add(new Text("Hello world from Vaadin"));
-        add(new Text("This is the second line"));
+        add(new Text("This is a text"));
 
         // We can use HTML and CSS related code to customize
         Span span = new Span("This is a span");
         span.getElement().getStyle().set("font-size", "23px");
         span.getElement().getStyle().set("font-weight", "bold");
-
-        add(new Text("This is a text"));
 
         Button button = new Button("Click me", event -> {
             add(new Text("Button has been clicked "));
@@ -34,12 +34,24 @@ public class MainView extends VerticalLayout {
             System.out.println("The value has been changed");
         });
 
+        Person person = new Person();
+
+        // data binding
+        Binder<Person> binder = new Binder<>();
+        binder.bind(nameField, Person::getName, Person::setName);
+
         Button saveButton = new Button("Save", event-> {
+            try {
+                binder.writeBean(person);
+                System.out.println(person);
+            }
+            catch(ValidationException e) {
+                throw new RuntimeException(e);
+            }
             add(new Text(nameField.getValue()));
         });
 
         add(span, button, nameField, saveButton);
-
     }
 
 
