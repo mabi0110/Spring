6ac6f6248c329171ecb7a66e59a9163c6348f614
mbiziewska <Kotmaaids2502!@>
2023-09-19 15:06:08 +0200

    replace label with text

--- vaadin-basics/src/main/java/com/example/vaadinbasics/MainView.java
+++ vaadin-basics/src/main/java/com/example/vaadinbasics/MainView.java
@@ -2,7 +2,6 @@ package com.example.vaadinbasics;
 
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -14,8 +13,8 @@ import com.vaadin.flow.router.Route;
 public class MainView extends VerticalLayout {
 
     public MainView(){
-        add(new Label("Hello world from Vaadin"));
-        add(new Label("This is the second line"));
+        add(new Text("Hello world from Vaadin"));
+        add(new Text("This is the second line"));
 
         // We can use HTML and CSS related code to customize
         Span span = new Span("This is a span");
