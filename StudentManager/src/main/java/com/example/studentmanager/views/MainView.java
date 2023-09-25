182714a03d0b880aa48062c453a57892dcf3bdb1
mabi0110 <biziewskamk@gmail.com>
2023-09-25 13:57:20 +0200

    add grid customization

--- StudentManager/src/main/java/com/example/studentmanager/views/MainView.java
+++ StudentManager/src/main/java/com/example/studentmanager/views/MainView.java
@@ -3,6 +3,8 @@ package com.example.studentmanager.views;
 import com.example.studentmanager.model.Status;
 import com.example.studentmanager.model.Student;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -22,15 +24,40 @@ public class MainView extends VerticalLayout {
         setAlignItems(Alignment.CENTER);
 
         createFieldVariables();
+        configureGrid();
 
         add(logoLayout, grid);
 
         loadStudents();
     }
 
+    private void configureGrid() {
+        grid.setSizeFull();
+        grid.setColumns("country", "zipCode");
+        grid.addColumn(Student::getName).setHeader("Name");
+        grid.addColumn(Student::getAge).setHeader("Age");
+        grid.addComponentColumn(s -> {
+            Icon icon;
+
+            if (s.getStatus().getName().equals("ACTIVE")) {
+                icon = VaadinIcon.CIRCLE.create();
+                icon.setColor("green");
+            } else if (s.getStatus().getName().equals("NOT ACTIVE")) {
+                icon = VaadinIcon.CLOSE_CIRCLE.create();
+                icon.setColor("red");
+            } else {
+                icon = VaadinIcon.CHECK_CIRCLE.create();
+                icon.setColor("orange");
+            }
+            return icon;
+        }).setHeader("Status");
+    }
+
     private void loadStudents() {
         List<Student> students = new ArrayList<>();
-        students.add(new Student("Adam", 23, 21123, "Poland", new Status("ACTIVE")));
+        students.add(new Student("Adam", 23, 11223, "PL", new Status("ACTIVE")));
+        students.add(new Student("Ala", 20, 12225, "PL", new Status("NOT ACTIVE")));
+        students.add(new Student("Ula", 26, 25312, "PL", new Status("ABSOLVED")));
         grid.setItems(students);
     }
 
