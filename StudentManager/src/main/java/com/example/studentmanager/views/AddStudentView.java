b9843baa99327952b9cd45d7a00521ababd5900d
mabi0110 <biziewskamk@gmail.com>
2023-09-26 14:23:12 +0200

    add student from

--- StudentManager/src/main/java/com/example/studentmanager/views/AddStudentView.java
+++ StudentManager/src/main/java/com/example/studentmanager/views/AddStudentView.java
@@ -1,7 +1,14 @@
 package com.example.studentmanager.views;
 
+import com.example.studentmanager.model.Status;
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -9,8 +16,44 @@ import com.vaadin.flow.router.Route;
 @Route(value = "add-student")
 public class AddStudentView extends VerticalLayout {
 
+    private TextField age;
+    private TextField zipCode;
+    private TextField name;
+    private TextField country;
+    private ComboBox<Status> status;
+    private LogoLayout image;
+    private Button save;
+    private Button close;
+
     public AddStudentView(){
-        add(new Button("Click me"));
+        setAlignItems(Alignment.CENTER);
+        createVariables();
+
+        add(image);
+        add(createFormLayout());
+    }
+
+    private Component createFormLayout() {
+        FormLayout formLayout = new FormLayout();
+        formLayout.add(name, age, country, zipCode, status, createButtons());
+        return formLayout;
+    }
+
+    private Component createButtons() {
+        save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        close.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+        return new HorizontalLayout(save, close);
+    }
+
+    private void createVariables() {
+        age = new TextField("Age");
+        name = new TextField("Name");
+        country = new TextField("Country");
+        zipCode = new TextField("Zip code");
+        status = new ComboBox<Status>("Status");
+        image = new LogoLayout();
+        save = new Button("Save");
+        close = new Button("Cancel");
     }
 
 }
